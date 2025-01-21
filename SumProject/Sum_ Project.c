5227bbbb2956fb031a2f2d341f42f0208f2ec363
Md-Saharia-Nafiz <nafizmdsaharia48@gmail.com>
2025-01-22 00:27:39 +0600

    Summation of numbers

---
+++ SumProject/Sum_ Project.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b,c;
+
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter value of b: ");
+    scanf("%d", &b);
+
+    printf("Enter value of c: ");
+    scanf("%d", &c);
+
+    printf("\n");
+
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a+b+c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a+b-c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a+b*c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a*b*c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a-b+c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a-b*c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a-b-c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a*b/c));
+
+    printf("Addition of %d,%d,%d is : %d \n", a,b,c, (a/b/c));
+
+    printf("Addition of %d,%d,%d is : %d", a,b,c, (a/b+c));
+
+
+    return 0;
+}
